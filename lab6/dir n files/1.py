cc2ede0cd66d4992b91c68df6453858da9b7b9ad
tamilana <tamilanakkali@gmail.com>
2025-03-02 22:48:23 +0500

    first task modified

--- lab6/dir n files/1.py
+++ lab6/dir n files/1.py
@@ -1,27 +1,23 @@
 import os
 
 def list_directories_files(path):
-    path = os.path.abspath(path)  # Преобразуем путь в абсолютный
-    entries = os.listdir(path)  # Получаем список файлов и папок в директории
-
-    directories = [entry for entry in entries if os.path.isdir(os.path.join(path, entry))]
-    files = [entry for entry in entries if os.path.isfile(os.path.join(path, entry))]
-
     print("Directories:")
-    for directory in directories:
-        print("   ", directory)
+    
+    for entry in os.listdir(path):
+        full_path = os.path.join(path, entry)
+        print("   ", full_path)  
 
-    print("\n Files:")
-    for file in files:
-        print("   ", file)
+    print("\nFiles:")
+    for entry in os.listdir(path):
+        if os.path.isfile(os.path.join(path, entry)):
+            print("   ", entry)
 
-    print("\n All directories and files:")
+    print("\nAll directories and files:")
     for root, dirs, files in os.walk(path):
         for dir in dirs:
-            print(os.path.join(root, dir))
+            print("   ",os.path.join(root, dir))
         for file in files:
-            print(os.path.join(root, file))
-
+            print("   ",os.path.join(root, file))
 
 path = "/Users/tamilana/Desktop/PP2/pp22/lab6/dir n files"
 
